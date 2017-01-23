4ac2a04fb74daf73fce3eb6e466a6a8760f94c7e
tomaszek92 <tomaszek92@gmail.com>
2017-01-23 09:24:51 +0100

    Add StringIntern

---
+++ Prezentacja/StringIntern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringIntern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "jakis_string";
+            string str2 = "jakis_string";
+            string str3 = new StringBuilder().Append("jakis").Append("_").Append("string").ToString();
+            string str4 = String.Intern(str3);
+
+            Console.WriteLine((object) str1 == (object) str2);
+            Console.WriteLine((object) str2 == (object) str3);
+            Console.WriteLine((object) str1 == (object) str4);
+
+            Console.ReadLine();
+        }
+    }
+}
